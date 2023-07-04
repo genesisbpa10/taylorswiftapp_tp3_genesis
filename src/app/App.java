c2759b8de0ade8dff880134c56c35627f03f5c41
Genesis Perez <genesis.perez@accenture.com>
2023-07-03 23:46:53 -0300

    agregando correcciones e interfaz

--- src/app/App.java
+++ src/app/App.java
@@ -5,46 +5,50 @@ import services.Discografia;
 import services.Recomendacion;
 import services.Lista;
 
-
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String opcion;
-
+        int opcion = 0;
         System.out.print("¡Bienvenido a la aplicación de Taylor Swift! Por favor, introduce tu nombre: ");
         String nombreUsuario = scanner.nextLine();
         System.out.println("¡Hola, " + nombreUsuario + "!");
 
         do {
-            System.out.println("\nSelecciona una opción:");
+            System.out.println("Selecciona una opción:");
             System.out.println("1. Recorrer la discografía");
             System.out.println("2. Crear una lista de favoritos");
             System.out.println("3. Generar una recomendación");
             System.out.println("4. Salir");
-            System.out.print("Ingresa la opción de tu preferencia: ");
-            
-            opcion = scanner.nextLine();
+            System.out.print("Opción: ");
+
+            String opcionStr = scanner.nextLine();
+
+            try {
+                opcion = Integer.parseInt(opcionStr);
+            } catch (NumberFormatException e) {
+                System.out.println("Opción inválida. Por favor, selecciona una opción válida.");
+                continue;
+            }
 
             switch (opcion) {
-                case "1":
-                    Discografia.recorrerDiscografia(); // Llama al método para recorrer la discografía
+                case 1:
+                    Discografia.recorrerDiscografia();
                     break;
-                case "2":
-                    Lista.crearListaFavoritos(); // Llama al método para crear una lista de favoritos
+                case 2:
+                    Lista.crearListaFavoritos();
                     break;
-                case "3":
-                    Recomendacion.generarRecomendacion(); // Llama al método para generar una recomendación
+                case 3:
+                    Recomendacion.generarRecomendacion();
                     break;
-                case "4":
-                    System.out.println("\n¡Hasta luego, " + nombreUsuario + "!"); // Mensaje de despedida al usuario
+                case 4:
+                    System.out.println("\n¡Hasta luego, " + nombreUsuario + "!");
                     break;
                 default:
                     System.out.println("Opción no válida. Por favor, selecciona una opción válida.");
                     break;
             }
+        } while (opcion != 4);
 
-        } while (!opcion.equals("4"));
-
-        scanner.close(); // Cierra el Scanner al salir del bucle
+        scanner.close();
     }
 }
--- src/services/Discografia.java
+++ src/services/Discografia.java
@@ -14,38 +14,39 @@ public class Discografia {
         boolean finalizar = false;
         int index = 0;
 
-        while (!finalizar && index < discografia.size()) {
-            Album album = discografia.get(index);
-            System.out.println("Título del álbum: " + album.getTituloAlbum());
-            System.out.println("Año de lanzamiento: " + album.getAnioLanzamiento());
-            System.out.println("Canciones:");
+        try {
+            while (!finalizar && index < discografia.size()) {
+                Album album = discografia.get(index);
+                System.out.println("Título del álbum: " + album.getTituloAlbum());
+                System.out.println("Año de lanzamiento: " + album.getAnioLanzamiento());
+                System.out.println("Canciones:");
 
-            ArrayList<String> canciones = album.getCancionesDelAlbum();
-            for (String cancion : canciones) {
-                System.out.println("- " + cancion);
-            }
+                ArrayList<String> canciones = album.getCancionesDelAlbum();
+                for (String cancion : canciones) {
+                    System.out.println("- " + cancion);
+                }
 
-            System.out.println("--------------------");
+                System.out.println("--------------------");
 
-            if (index < discografia.size() - 1) {
-                System.out.print("¿Deseas continuar con el siguiente álbum? (S/N): ");
-                String opcion = scanner.nextLine();
-                if (opcion.equalsIgnoreCase("N")) {
+                if (index < discografia.size() - 1) {
+                    System.out.print("¿Deseas continuar con el siguiente álbum? (S/N): ");
+                    String opcion = scanner.nextLine();
+                    if (opcion.equalsIgnoreCase("N")) {
+                        finalizar = true;
+                    }
+                } else {
+                    System.out.println("Has alcanzado el final de la discografía.");
                     finalizar = true;
                 }
-            } else {
-                System.out.println("Has alcanzado el final de la discografía.");
-                finalizar = true;
+
+                index++;
             }
-
-            index++;
+        } catch (Exception e) {
+            // Manejo de la excepción
+            System.out.println("Ocurrió un error al recorrer la discografía: " + e.getMessage());
+        } finally {
+            scanner.close();
+            System.exit(0); // Finaliza la aplicación
         }
-
-        // Agregamos una pausa para que el usuario pueda leer la información antes de volver al menú principal
-        scanner.nextLine();
-        System.out.print("Presiona Enter para volver al menú principal...");
-
-        // Cerramos el scanner
-        scanner.close();
     }
 }
